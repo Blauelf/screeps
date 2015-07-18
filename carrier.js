b9ff1151b9e173a116f75a9964869477bd21318b
Blauelf <jojo314@web.de>
2015-07-18 14:31:05 +0200

    carrier should sometimes forget who they are working for (and choose a new employer) until I implement demand-based creep creation

--- carrier.js
+++ carrier.js
@@ -6,27 +6,30 @@
  * var mod = require('carrier'); // -> 'a thing'
  */
 module.exports = function (creep) {
-	if(creep.energy === 0) {
-	    if (creep.memory.target === null || !(creep.memory.target in Game.creeps)) {
-	        var creeps = creep.room.find(FIND_MY_CREEPS);
-	        var besttarget = null;
-	        var maxwaiting = 0;
-	        for (var i=0;i<creeps.length;i++) {
-	            if ((creeps[i].memory.role==='harvester') && ('waiting' in creeps[i].memory) && (creeps[i].memory.waiting>maxwaiting)) {
-	                besttarget=creeps[i].name;
-	                maxwaiting=creeps[i].memory.waiting;
-	            }
-	        }
-	        creep.memory.target=besttarget;
-	    }
-		if (creep.memory.target !== null) {
-		    creep.moveTo(Game.creeps[creep.memory.target]);
-		} else {
-		    creep.move(1+Math.floor(8*Math.random()));
+		if(creep.energy === 0) {
+				if (100*Math.random()<1) {
+						creep.memory.target=null;
+				}
+				if (creep.memory.target === null || !(creep.memory.target in Game.creeps)) {
+						var creeps = creep.room.find(FIND_MY_CREEPS);
+						var besttarget = null;
+						var maxwaiting = 0;
+						for (var i=0;i<creeps.length;i++) {
+								if ((creeps[i].memory.role==='harvester') && ('waiting' in creeps[i].memory) && (creeps[i].memory.waiting>maxwaiting)) {
+										besttarget=creeps[i].name;
+										maxwaiting=creeps[i].memory.waiting;
+								}
+						}
+						creep.memory.target=besttarget;
+				}
+				if (creep.memory.target !== null) {
+						creep.moveTo(Game.creeps[creep.memory.target]);
+				} else {
+						creep.move(1+Math.floor(8*Math.random()));
+				}
 		}
-	}
-	else {
-		creep.moveTo(Game.spawns.Spawn1);
-		creep.transferEnergy(Game.spawns.Spawn1);
-	}
+		else {
+				creep.moveTo(Game.spawns.Spawn1);
+				creep.transferEnergy(Game.spawns.Spawn1);
+		}
 }
